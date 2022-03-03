5702c3c0d1c1ce4263f7a491ff2a94ad22ac3a25
vigge93 <victarv@gmail.com>
2022-03-03 09:51:23 +0100

    Started assignment 3
    Co-authored-by: Ahmad Al-mashahedi <Blueflamefirez@users.noreply.github.com>

---
+++ Assignment3/test_classes/E.java
@@ -0,0 +1,9 @@
+class Minimal {
+  public static void main(String[] a){
+    {
+	    System.out.println((4*2)+10-2*6+(4-1)*2/2);
+      System.out.println(true == (!true && false) || 10 > 1 && 1 < 10);
+      a[3 + 4] = a[4 / 3 + 5];
+    }
+  }
+}
